32ff85580617fbf6d5d2ef1994bc25ca6073e700
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-06 13:52:58 -0500

    Giving user feedback

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import speech_recognition as sr
+from time import ctime
+
 r = sr.Recognizer()
 
-# Set to device index 2 for the stereo mix use, instead
-# of microphone
-with sr.Microphone(device_index=2) as source:
-    print("Say something!")
-    audio = r.listen(source)
-    voice_data = r.recognize_google(audio)
-    print(voice_data)
+
+# Record audio via speech recognition library
+def record_audio():
+
+    # Set to device index 2 for the stereo mix use, instead
+    # of microphone
+    with sr.Microphone(device_index=2) as source:
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print('Sorry, I didn\'t get that')
+        except sr.RequestError:
+            print("Sorry, I am not active")
+        return voice_data
+
+
+# Respond to user voice input
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        print("My name is giogle")
+    if 'what time is it' in voice_data:
+        print("It is", ctime())
+
+
+print("How can I help you?")
+voice_data = record_audio()
+respond(voice_data)
