89368d6e24768617a07e2f9b1bad81b219d62dea
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-06 14:19:53 -0500

    Added exit condition on command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
 import webbrowser
-from time import ctime
+import time
 
 r = sr.Recognizer()
 
@@ -29,15 +29,26 @@ def respond(voice_data):
     if 'what is your name' in voice_data:
         print("My name is giogle")
     if 'what time is it' in voice_data:
-        print("It is", ctime())
+        print("It is", time.ctime())
     if 'search' in voice_data:
         search = record_audio('What do you want to search for?')
         # Search google for related request
         url = 'https://google.com/search?q=' + search
         print('Here is what I found for '+search)
         webbrowser.get().open(url)
+    if 'find location' in voice_data:
+        location = record_audio('What is the location?')
+        # Search google for related location request
+        url = 'https://google.nl/maps/place/' + location + '/&amp;'
+        webbrowser.get().open(url)
+        print('Here is the location '+location)
+    if 'exit' in voice_data:
+        exit()
 
 
+# Sleep keeps voice channel open
+time.sleep(1)
 print("How can I help you?")
-voice_data = record_audio()
-respond(voice_data)
+while 1:
+    voice_data = record_audio()
+    respond(voice_data)
