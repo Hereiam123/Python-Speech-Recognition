8645cb58de049a2abeac6180bba73947b29007a9
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-06 13:58:48 -0500

    Adding in google search features for search requests

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import speech_recognition as sr
+import webbrowser
 from time import ctime
 
 r = sr.Recognizer()
 
 
 # Record audio via speech recognition library
-def record_audio():
+def record_audio(ask=False):
 
     # Set to device index 2 for the stereo mix use, instead
     # of microphone
     with sr.Microphone(device_index=2) as source:
+        if ask:
+            print("You asked to search.\n", ask)
         audio = r.listen(source)
         voice_data = ''
         try:
@@ -27,6 +30,12 @@ def respond(voice_data):
         print("My name is giogle")
     if 'what time is it' in voice_data:
         print("It is", ctime())
+    if 'search' in voice_data:
+        search = record_audio('What do you want to search for?')
+        # Search google for related request
+        url = 'https://google.com/search?q=' + search
+        print('Here is what I found for '+search)
+        webbrowser.get().open(url)
 
 
 print("How can I help you?")
